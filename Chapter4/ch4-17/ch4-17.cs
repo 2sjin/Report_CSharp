4bef9ae3c3049689489812255c46397036998128
options3224@gmail.com <options3224@gmail.com>
2022-10-10 01:35:58 +0900

    예제 4-24, 연습문제 ch 4-17 완료

---
+++ Chapter4/ch4-17/ch4-17.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch4_17 {
+    class Vector {
+        private int[] v;
+
+        // 생성자
+        public Vector(int size) {
+            v = new int[size];
+        }
+
+        // 인덱서
+        public int this[int index] {
+            get { return v[index]; }
+            set { v[index] = value; }
+        }
+
+        // 연산자 중복(++): 모든 원소 1씩 증가
+        static public Vector operator ++(Vector vector) {
+            Console.WriteLine("클래스 Vector의 ++ 연산자가 호출됨");
+            for (int i = 0; i < vector.v.Length; i++)
+                vector[i]++;
+            return vector;
+        }
+
+        // 연산자 중복(--): 모든 원소 1씩 감소
+        static public Vector operator --(Vector vector) {
+            Console.WriteLine("클래스 Vector의 -- 연산자가 호출됨");
+            for (int i = 0; i < vector.v.Length; i++)
+                vector[i]--;
+            return vector;
+        }
+
+    }
+
+    class Program {
+        static int VECTOR_SIZE = 5;
+
+        // 벡터 출력 메소드
+        static void PrintVector(Vector vector) {
+            for (int i = 0; i < VECTOR_SIZE; i++)
+                Console.Write(vector[i] + " ");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args) {
+            // 벡터 객체 생성
+            Vector vector = new Vector(VECTOR_SIZE);
+
+            // 벡터 초기화
+            for (int i = 0; i < VECTOR_SIZE; i++)
+                vector[i] = 10 + i;
+
+            PrintVector(vector);    // 벡터 출력
+            vector++;   // 벡터 ++ 연산 수행
+            PrintVector(vector);    // 벡터 출력
+            vector--;   // 벡터 -- 연산 수행
+            PrintVector(vector);    // 벡터 출력
+        }
+    }
+}
--- Chapter4/ex4-24/OperatorOverloadingApp.cs
+++ Chapter4/ex4-24/OperatorOverloadingApp.cs
@@ -5,8 +5,43 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ex4_24 {
-    internal class OperatorOverloadingApp {
-        static void Main(string[] args) {
+    class Complex {
+        private double realPart;    // 실수부
+        private double imagePart;   // 허수부
+
+        public Complex() {
+            this.realPart = 0;
+            this.imagePart = 0;
+        }
+
+        public Complex(double rVal, double iVal) {
+            this.realPart = rVal;
+            this.imagePart = iVal;
+        }
+
+        // 연산자 중복(리턴타입: Complex)
+        public static Complex operator +(Complex x1, Complex x2) {
+            Complex temp = new Complex();   // new Complex(0, 0)
+            temp.realPart = x1.realPart + x2.realPart;
+            temp.imagePart = x1.imagePart + x2.imagePart;
+            return temp;
+        }
+
+        // ToString() 재정의
+        // override 하지 않으면 프로젝트명.객체타입(deu20194146.Complex) 출력됨
+        override public string ToString() {
+            return "(" + this.realPart + " + " + this.imagePart + "i)";
         }
     }
+
+    class OperatorOverloadingApp {
+        static void Main(string[] args) {
+            Complex c1 = new Complex(1, 2);
+            Complex c2 = new Complex(3, 4);
+            Complex c = c1 + c2;
+
+            Console.WriteLine("{0} + {1} = {2}", c1, c2, c);
+        }
+    }
+
 }
