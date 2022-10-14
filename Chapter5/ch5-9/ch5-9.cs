e54334fe47a0cda0fdc880c97588f7322dca846c
options3224@gmail.com <options3224@gmail.com>
2022-10-15 00:13:18 +0900

    연습문제 5-스택 메인 메소드 코드 함수화

--- Chapter5/ch5-9/ch5-9.cs
+++ Chapter5/ch5-9/ch5-9.cs
@@ -10,6 +10,7 @@ namespace ch5_9 {
         int NumOfElements();
     }
 
+    // 파생 클래스: 스택
     class Stack : IOperation {
         string[] stack;     // 스택
         int sp = -1;        // 스택 포인터
@@ -35,7 +36,7 @@ namespace ch5_9 {
         // 메소드: 주어진 원소의 존재여부 반환
         public bool Search(string str) {
             bool result = false;
-            for (int i = 0; i < sp; i++) {
+            for (int i = 0; i <= sp; i++) {
                 if (str.Equals(stack[i])) {
                     result = true;
                     break;
@@ -57,22 +58,20 @@ namespace ch5_9 {
     }
     
 
-
+    // 클래스: 메인 클래스
     internal class Program {
+        // 메소드: 스택 원소 개수 및 탑 원소 출력
         static void PrintStackInfo(Stack stack) {
             Console.WriteLine("{0}개 저장됨, top={1}", stack.NumOfElements(), stack.GetCurrentElt());
         }
 
-        static void Main(string[] args) {
-            string inputData, searchData;
-
-            Stack stack = new Stack();
-            PrintStackInfo(stack);
+        // 메소드: 스택 PUSH 반복
+        static void PushStack(Stack stack) {
+            string inputData;
 
             Console.WriteLine("\n스택에 PUSH할 문자열을 입력하세요. (종료하려면 입력 없이 [Enter])");
-            
-            // PUSH 반복
-            while(true) {
+
+            while (true) {
                 Console.Write("PUSH >> ");
                 inputData = Console.ReadLine();
                 if (inputData.Equals(""))   // 공백 입력 시
@@ -80,10 +79,14 @@ namespace ch5_9 {
                 stack.Insert(inputData);    // PUSH
                 PrintStackInfo(stack);
             }
+        }
+
+        // 메소드: 스택 검색 반복
+        static void SearchStack(Stack stack) {
+            string searchData;
 
             Console.WriteLine("\n검색할 문자열을 입력하세요. (종료하려면 입력 없이 [Enter])");
 
-            // SEARCH 반복
             while (true) {
                 Console.Write("SEARCH >> ");
                 searchData = Console.ReadLine();
@@ -94,15 +97,26 @@ namespace ch5_9 {
                 else
                     Console.WriteLine(searchData + " : Stack 내에 없습니다.");
             }
+        }
 
+        // 메소드: 스택 POP 반복
+        static void PopStack(Stack stack) {
             Console.WriteLine("");
 
-            // POP 반복
             while (stack.NumOfElements() > 0) {
                 Console.WriteLine("POP >> " + stack.delete());
                 PrintStackInfo(stack);
             }
+        }
 
+
+        // 메소드: Main
+        static void Main(string[] args) {
+            Stack stack = new Stack();
+            PrintStackInfo(stack);
+            PushStack(stack);
+            SearchStack(stack);
+            PopStack(stack);
         }
     }
 }
