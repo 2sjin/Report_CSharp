c4029180dccca7afd0e5b39a3b24c0cc422bb7bc
options3224@gmail.com <options3224@gmail.com>
2022-10-14 16:04:13 +0900

    연습문제 ch4-11 일부 완료, ch4-13 완료

--- Chapter4/ch4-11/ch4-11.cs
+++ Chapter4/ch4-11/ch4-11.cs
@@ -5,8 +5,43 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ch4_11 {
+    class Fraction {
+        public int numerator;       // 분자
+        public int denominator;     // 분모
+
+        // 생성자: 매개변수 없는 생성자
+        public Fraction() {
+            this.numerator = 1;
+            this.denominator = 1;
+        }
+
+        // 생성자: 한 개의 정수를 받아 초기화
+        public Fraction(int numerator) {
+            this.numerator = numerator;
+            this.denominator = 1;
+        }
+
+        // 생성자: 두 개의 정수를 받아 초기화
+        public Fraction(int numerator, int denominator) {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        // 메소드 재정의(클래스명 대신 재정의한 문자열을 리턴함)
+        override public string ToString() {
+            return this.numerator + "/" + this.denominator;
+        }
+
+        // 메소드: 최대공약수
+        override public void GCD() {
+            return void;
+        }
+    }
+
     internal class Program {
         static void Main(string[] args) {
+            Fraction f = new Fraction(2, 3);
+            Console.WriteLine(f);
         }
     }
 }
--- Chapter4/ch4-13/ch4-13.cs
+++ Chapter4/ch4-13/ch4-13.cs
@@ -5,8 +5,69 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ch4_13 {
+    class Stack {
+        private int[] stack;    // 스택
+        private int sp = -1;    // 스택 포인터
+
+        // 디폴트 생성자(스택의 크기 100)
+        public Stack() {
+            stack = new int[100];
+        }
+
+        // 생성자
+        public Stack(int size) {
+            stack = new int[size];
+        }
+
+        // Push 메소드
+        public void Push(int data) {
+            sp++;
+            stack[sp] = data;
+        }
+
+        // Pop 메소드
+        public int Pop() {
+            sp--;
+            return stack[sp+1];
+        }
+
+        // 스택이 비었는지 확인
+        public bool IsEmpty() {
+            return sp <= -1;     // 스택 포인터가 -1이면 비어있는 상태
+        }
+
+    }
+
     internal class Program {
         static void Main(string[] args) {
+            int inputNum = 0;
+
+            // 스택 객체 생성
+            Stack stack = new Stack(10);
+
+            // 정수 0을 입력받을 때 까지 입력 및 Push 반복
+            while (true) {
+                // 정수 입력
+                try {
+                    Console.Write("Push할 정수: ");
+                    inputNum = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e) {
+                    Console.WriteLine("[입력 오류] 정수를 다시 입력해주세요.");
+                    continue;
+                }
+
+                // 0 입력 시 스택 Push 종료
+                if (inputNum == 0) break;
+
+                // 입력한 정수를 스택에 Push
+                stack.Push(inputNum);                
+            }
+
+            // 입력한 정수를 역순으로 Pop하여 출력함
+            while (!stack.IsEmpty()) {      // 스택이 비어있지 않으면 계속 반복함
+                Console.WriteLine(stack.Pop());
+            }
         }
     }
 }
