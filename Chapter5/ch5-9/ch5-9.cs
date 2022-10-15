44a2381e12e99b3cb9d47438c9105abb4bd7faf8
options3224@gmail.com <options3224@gmail.com>
2022-10-15 11:14:04 +0900

    연습문제 5-9를 인터페이스, 엔티티 클래스, 제어 클래스, 경계 클래스로 분류함

--- Chapter5/ch5-9/ch5-9.cs
+++ Chapter5/ch5-9/ch5-9.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 
+// 코드가 길어지는 관계로 아래와 같이 클래스를 나누어 보았음
+// 인터페이스, 엔티티 클래스(스택/큐), 제어 클래스(스택/큐), 경계 클래스
+
 namespace ch5_9 {
     // 인터페이스
     interface IOperation {
@@ -10,13 +13,13 @@ namespace ch5_9 {
         int NumOfElements();
     }
 
-    // 파생 클래스: 스택
-    class Stack : IOperation {
+    // 엔티티 클래스: 스택 (인터페이스의 파생 클래스)
+    class StackEntity : IOperation {
         string[] stack;     // 스택
         int sp = -1;        // 스택 포인터
 
         // 생성자
-        public Stack(int i=100) {
+        public StackEntity(int i=100) {
             Console.WriteLine("크기가 {0}인 스택 생성됨.", i);
             stack = new string[i];
         }
@@ -57,16 +60,18 @@ namespace ch5_9 {
         }
     }
     
+    // 제어 클래스: 스택(엔티티 클래스)과 UI(경계 클래스)를 중간에서 제어함
+    class StackController {
+        // 자동 프로퍼티
+        StackEntity stack = new StackEntity();
 
-    // 클래스: 메인 클래스
-    internal class Program {
         // 메소드: 스택 원소 개수 및 탑 원소 출력
-        static void PrintStackInfo(Stack stack) {
+        public void PrintStackInfo() {
             Console.WriteLine("{0}개 저장됨, top={1}", stack.NumOfElements(), stack.GetCurrentElt());
         }
 
         // 메소드: 스택 PUSH 반복
-        static void PushStack(Stack stack) {
+        public void PushStack() {
             string inputData;
 
             Console.WriteLine("\n스택에 PUSH할 문자열을 입력하세요. (종료하려면 입력 없이 [Enter])");
@@ -77,12 +82,12 @@ namespace ch5_9 {
                 if (inputData.Equals(""))   // 공백 입력 시
                     break;                  // 반복 종료
                 stack.Insert(inputData);    // PUSH
-                PrintStackInfo(stack);
+                PrintStackInfo();
             }
         }
 
         // 메소드: 스택 검색 반복
-        static void SearchStack(Stack stack) {
+        public void SearchStack() {
             string searchData;
 
             Console.WriteLine("\n검색할 문자열을 입력하세요. (종료하려면 입력 없이 [Enter])");
@@ -100,23 +105,26 @@ namespace ch5_9 {
         }
 
         // 메소드: 스택 POP 반복
-        static void PopStack(Stack stack) {
+        public void PopStack() {
             Console.WriteLine("");
 
             while (stack.NumOfElements() > 0) {
                 Console.WriteLine("POP >> " + stack.delete());
-                PrintStackInfo(stack);
+                PrintStackInfo();
             }
         }
+    }
 
 
+    // 경계 클래스: 사용자 인터페이스
+    class BoundaryApp {
         // 메소드: Main
         static void Main(string[] args) {
-            Stack stack = new Stack();
-            PrintStackInfo(stack);
-            PushStack(stack);
-            SearchStack(stack);
-            PopStack(stack);
+            StackController stackController = new StackController();
+            stackController.PrintStackInfo();
+            stackController.PushStack();
+            stackController.SearchStack();
+            stackController.PopStack();
         }
     }
 }
